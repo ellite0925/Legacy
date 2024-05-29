2163cda98309657d19362285ad96a89b83b61d45
ellite0925 <ellite0925@gmail.com>
2024-05-28 23:51:00 -0700

    +routing

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,20 @@ import HomePage from './pages/HomePage';
 import InfoCollection from './pages/InfoCollection';
 import SingleStamp from './pages/SingleStamp';
 
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 function App() {
   return (
     <>
-      <HomePage />
-      {/* <InfoCollection /> */}
-      {/* <FullCollection /> */}
-      {/* <SingleStamp /> */}
-      {/* <AsgneToken /> */}
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/info_collection" element={<InfoCollection />} />
+          <Route path="/full_collection" element={<FullCollection />} />
+          <Route path="/single_stamp" element={<SingleStamp />} />
+          <Route path="/asgne_token" element={<AsgneToken />} />
+        </Routes>
+      </Router>
     </>
   )
 }
